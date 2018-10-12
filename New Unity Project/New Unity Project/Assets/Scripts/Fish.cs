f5c5c932cbf27235fd75803f2e7544927416c571
Mercedie <10830390@my.uvu.edu>
2018-10-12 11:47:02 -0600

    Random Stuff

---
+++ New Unity Project/New Unity Project/Assets/Scripts/Fish.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Fish : MonoBehaviour
+{
+
+	// Update is called once per frame
+	void Update ()
+	{
+		print("I am a sparkly fish");
+
+		}
+		
+
+	}
+
+
+
+
